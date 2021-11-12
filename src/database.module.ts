b80b76ae0957f9740e7137b1e5d85933d4113618
abhishek <abhishek@thinksys.com>
2021-11-12 10:16:03 +0530

    database url added

--- src/database.module.ts
+++ src/database.module.ts
@@ -14,11 +14,6 @@ import { RepoEntity } from './entities/repo.entity';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         url: process.env.DATABASE_URL,
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
         entities: [
           User,
           FileEntity,
